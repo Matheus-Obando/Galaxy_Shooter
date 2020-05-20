3e9b68fd6ff11a31fb2054583a8ea99420a9d691
Matheus-Obando <espcs117@gmail.com>
2020-05-20 06:43:38 -0300

    Created Game Main Menu

---
+++ Assets/Game/Scripts/Main_Menu/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public void OnSinglePlayerClicked()
+	{
+		Debug.Log("Loading Single Player Mode...");
+		SceneManager.LoadScene("Single_Player", LoadSceneMode.Single);
+		
+	}
+
+	public void OnCoopClicked()
+	{
+		Debug.Log("Loading Cooperative Mode...");
+		SceneManager.LoadScene("Co-Op_Mode", LoadSceneMode.Single);
+	}
+}
